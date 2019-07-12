c5edc07f9be00134babf25e4bcbe895df44067b9
Francis Avancena <foavancena@gmail.com>
2019-07-12 08:14:05 +0800

    Cookie implementation of themes

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,24 +2,30 @@
 
  function themeChange(){
     let theme = document.querySelector('.theme-icon');
-    if(document.documentElement.getAttribute('data-theme') === "light"){
+
+    if(document.cookie.includes('data-theme=light')){
+        document.documentElement.setAttribute('data-theme', 'light');
         document.querySelectorAll('.ui.button').forEach(function(button){
             button.classList.remove('inverted');
         });
     } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
         document.querySelectorAll('.ui.button').forEach(function(button){
             button.classList.add('inverted');
-        });
+        }); 
     }
+
     theme.addEventListener('click',function(e){
         if(document.documentElement.getAttribute('data-theme') === "light"){
             trans();
+            document.cookie = "data-theme=dark";
             document.documentElement.setAttribute('data-theme', 'dark');
             document.querySelectorAll('.ui.button').forEach(function(button){
                 button.classList.add('inverted');
             });
         } else {
             trans();
+            document.cookie = "data-theme=light";
             document.documentElement.setAttribute('data-theme', 'light');
             document.querySelectorAll('.ui.button').forEach(function(button){
                 button.classList.remove('inverted');
