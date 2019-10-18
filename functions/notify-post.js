1ba6d5417eff8d944f0330ef940f7fc03430c282
Francis Oliver Avanceña <foavancena@gmail.com>
2019-10-18 03:04:24 +0000

    Add netlify functions

---
+++ functions/notify-post.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+import faunadb from 'faunadb'
+const q = faunadb.query
+const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+
+exports.handler =  (event, context, callback)=>{
+    return client.query(q.Get(q.Match(q.Index("pushSubscriptions_by_id"), "1")))
+    .then(response =>{
+        console.log(reponse)
+        return callback(null,{
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        });
+    }).catch(error =>{
+        console.log(error)
+        return callback(null,{
+            statusCode: 400,
+            body: JSON.stringify(error)
+        });
+    });
+}
+
+// client.query(q.Get(q.Match(q.Index("pushSubscriptions_by_id"), "1"))).then(res => console.log(res)).catch(err => console.log(err));
---
+++ functions/subscribe.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const faunadb = require('faunadb');
+const q = faunadb.query
+const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+
+exports.handler =  (event, context, callback)=>{
+    return client.query(q.Create(q.Collection("pushSubscriptions"), { data: { id: "2"} })).then(response => {
+        console.log(response);
+        return callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        })
+    });
+}
+
+// client.query(q.Create(q.Collection("pushSubscriptions"), { data: { id: "2"} })).then(response => console.log(response)).catch(err=>console.log(err));
